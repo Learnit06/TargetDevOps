f66e3332aa6e589c6be0e8d33433161253495eb1
Ubuntu <azureuser@FirstVM.spugjruqv0tevngygc0fdkt54e.bx.internal.cloudapp.net>
2024-05-16 16:31:12 +0000

    removed the access key and saved it in the local Terraform directory

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform{
         storage_account_name = "trainingstorage06"
         container_name = "backend"
         key = "terraform.tfstate"
-        access_key = "oyQRkncNQpyKwRjo/H1ml14IfRqq4KS5+tKlnEhaAsVUMm8OJMBBZX205MC6jIkfFbhL0cAZWyOd+AStAOFbAg=="
+        access_key = ""
     }
 }
